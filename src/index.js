5b3d80999a143fd952a127942ed47ed9f465a118
Andrespg14 <anpergar14@gmail.com>
2023-12-15 14:27:14 +0100

    cambio

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
 // Aqui se encuentra el codigo del servidor
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT;
 
 // imports middleware (MODIFICAR)
 const userRoutes = require("./routes/user");
 
 // middleware (MODIFICAR)
 app.use(express.json());
-app.use('/users', userRoutes)
+app.use("/users", userRoutes);
 
 // routes
-app.get('/', (req, res) => {
-    res.send("Welcome to my API")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to my API");
+});
 
 // mongodb conection
-mongoose.connect(
-    "mongodb+srv://Profesor:2emg0lp1WJBtyEdG@exameniweb.syarvz8.mongodb.net/?retryWrites=true&w=majority")
-    .then(() => console.log("Connected to MongoDB Atlas"))
-    .catch((error) => console.error(error));
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((error) => console.error(error));
 
-app.listen(port, () => console.log('server listening on port', port))
+app.listen(port, "https://iweb-2324-server.vercel.app/", () =>
+  console.log("server listening on port", port)
+);
